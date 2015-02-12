ab58f424924273b50e1d43324f6abf3cc87e27ec
Joel Schow <jwschow@ucsd.edu>
2015-02-12 15:16:50 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -25,8 +25,13 @@ function addProjectDetails(e) {
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
 	console.log("User clicked on project " + idNumber);
+	$.get("/project/"+idNumber, function(result){
+		var detailHTML = '<img src="'+result["image"]+'" class="detailsImage"/>';
+		detailHTML += '<p>'+result["title"]+'</p><p><small>'+result["date"]+'</small></p>';
+		detailHTML += result["summary"];
+		$("#"+projectID).find(".thumbnail").find(".details").html(detailHTML);
+	});
 }
 
 /*
@@ -34,5 +39,13 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
+	$.get("/palette",function(result){
+		var colors = result["hex"];
+		$('body').css('background-color', colors[0]);
+		$('.thumbnail').css('background-color', colors[1]);
+		$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+		$('p').css('color', colors[3]);
+		$('.project img').css('opacity', .75);
+	});
 	console.log("User clicked on color button");
 }
